e849e8b5e424c1f41119db2dd6ddf9db286a8718
HieuLe <hieulcgl@gmail.com>
2025-07-16 05:03:59 +0700

    update font

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Poppins'),
+      theme: ThemeData(fontFamily: 'Inter'),
       home: const IntroducePage(),
     );
   }
